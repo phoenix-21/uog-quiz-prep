469278a584cce34575bec45ae2bc9ef6edd29c80
phoenix-21 <lunardrift2@gmail.com>
2026-03-30 13:32:25 +0500

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "Academic Quiz",
+  description: "Minimal Quiz App",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className="bg-gray-50 text-gray-900">
+        {children}
+      </body>
+    </html>
+  );
+}
